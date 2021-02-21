19dc1b643ca714ab92a44271f635a4540c8905c7
Marcos Rodriguez <marcoszas@gmail.com>
2021-02-21 16:37:11 +0100

    Added mini graphics and favorite button for charts.html

--- Template/dist/assets/demo/chart-area-badger.js
+++ Template/dist/assets/demo/chart-area-badger.js
@@ -10,7 +10,7 @@ var myLineChart2 = new Chart(chartbadger, {
         labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
         datasets: [{
             label: "Sessions",
-            lineTension: 0.3,
+            lineTension: 0,
             backgroundColor: "rgba(113, 214, 6 ,0.6)",
             borderColor: "rgba(88, 161, 11 ,1)",
             pointRadius: 5,
