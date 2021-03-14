77495c1ebef88dd5b868cf9b8552f08cbbce20ab
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-14 12:54:38 +0100

    Commented code

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/UserController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.practicaweb.practicadaw.controller;
 
-import com.practicaweb.practicadaw.model.User;
+//import com.practicaweb.practicadaw.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- Backend/src/main/java/com/practicaweb/practicadaw/repository/UserRepository.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.practicaweb.practicadaw.repository;
 
-public class UserRepository {
-}
+import org.apache.catalina.User;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+//public interface UserRepository extends JpaRepository<User, String> {
+//    List<User> findByRole(int rol);
+//
+//}
