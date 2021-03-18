df110b555994d5cc1b0fa96bb7554cf16eea9e2f
Adrian <adrianlopezcouso@gmail.com>
2021-03-18 19:09:52 +0100

    intento guardar comentario

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/CommentController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/CommentController.java
@@ -1,4 +1,22 @@
-package com.practicaweb.practicadaw.controller;
-
-public class CommentController {
-}
+//package com.practicaweb.practicadaw.controller;
+//
+//import com.practicaweb.practicadaw.model.Comment;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class CommentController {
+//    @PostMapping("/guardarDiscussion")
+//    public String guardarComentario(Model model, Comment comentario){
+//        model.addAttribute("comentario", comentario);
+//
+//        return "pruebaComentario";
+//    }
+//    public String guardarComentario(Model model, @RequestParam String testTitle,  @RequestParam String testDescription){
+//        model.addAttribute("description", testDescription);
+//        model.addAttribute("titulo", testTitle);
+//
+//        return "pruebaComentario";
+//    }
+//}
