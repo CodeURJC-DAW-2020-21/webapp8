6a685dab5af1f90e0d8d4da54ff8348bb1ed20ff
Adrian <adrianlopezcouso@gmail.com>
2021-03-19 10:44:04 +0100

    Controlador del comentario

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/CommentController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/CommentController.java
@@ -1,22 +1,25 @@
-//package com.practicaweb.practicadaw.controller;
-//
-//import com.practicaweb.practicadaw.model.Comment;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//public class CommentController {
-//    @PostMapping("/guardarDiscussion")
+package com.practicaweb.practicadaw.controller;
+
+import com.practicaweb.practicadaw.model.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CommentController {
+    @PostMapping("/guardarDiscussion")
+    public String guardarComentario(@RequestParam String testTitle,  @RequestParam String testDescription){
+        Comment comment = new Comment();
+        //comment.setUser();
+        comment.setTitle(testTitle);
+        comment.setDescription(testDescription);
+        //comment.setRegistrationDate();
+        return "pruebaComentario";
+
 //    public String guardarComentario(Model model, Comment comentario){
 //        model.addAttribute("comentario", comentario);
 //
 //        return "pruebaComentario";
 //    }
-//    public String guardarComentario(Model model, @RequestParam String testTitle,  @RequestParam String testDescription){
-//        model.addAttribute("description", testDescription);
-//        model.addAttribute("titulo", testTitle);
-//
-//        return "pruebaComentario";
-//    }
-//}
+    }
+}
