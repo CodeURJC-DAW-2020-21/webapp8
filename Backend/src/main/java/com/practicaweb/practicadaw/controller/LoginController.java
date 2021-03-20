857652fa1a91c18eebd9fdb21bcf28ceb59b45a3
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-20 18:16:11 +0100

    Fixed failed login

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/LoginController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.practicaweb.practicadaw.Service.UserService;
 import com.practicaweb.practicadaw.auxClasses.AuxUser;
 import com.practicaweb.practicadaw.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +20,11 @@ public class LoginController {
         this.userService = userService;
     }
 
-    @PostMapping("/checkLogin")
+    @GetMapping("/checkLogin")
     public String loginUser (@RequestParam("email") String email, @RequestParam("password") String password, HttpServletRequest request){
         User actualUser = userService.selectByEmail(email);
+        if (actualUser == null)
+            return "loginError";
         if (AuxUser.verificationEmail(actualUser.getEmail(), email) && AuxUser.verificationPassword(actualUser.getPassword(), password)){
             HttpSession mysession= request.getSession(true);
             mysession.setAttribute("user",actualUser);
