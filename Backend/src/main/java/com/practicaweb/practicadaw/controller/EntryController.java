bba139f64173e02e35be6c0afcee12cf08c6b750
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-20 19:45:14 +0100

    Now the entries are linked to the users that created them

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/EntryController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/EntryController.java
@@ -3,10 +3,14 @@ package com.practicaweb.practicadaw.controller;
 import com.practicaweb.practicadaw.Service.EntryService;
 import com.practicaweb.practicadaw.Service.UserService;
 import com.practicaweb.practicadaw.model.Entry;
+import com.practicaweb.practicadaw.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class EntryController {
@@ -21,8 +25,10 @@ public class EntryController {
 
     // New entry
     @PostMapping("/newEntry")
-    public String createEntry(@ModelAttribute Entry entry){
-//        userService.selectById(id);
+    public String createEntry(@ModelAttribute Entry entry, HttpServletRequest request){
+        HttpSession misession = (HttpSession) request.getSession();
+        User userActual = (User)misession.getAttribute("user");
+        entry.setUser(userActual);
         entryService.save(entry);
         return "redirect:/";
     }
