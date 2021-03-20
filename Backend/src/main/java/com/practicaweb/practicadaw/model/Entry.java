ac0abb025f980b7679c88d09946e23ccfcbad4b4
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-20 19:44:40 +0100

    New number of visits field added

--- Backend/src/main/java/com/practicaweb/practicadaw/model/Entry.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/model/Entry.java
@@ -12,11 +12,21 @@ public class Entry {
     private String title;
     @Column(length = 45, nullable = false)
     private String description;
+    @Column(length = 45, nullable = false)
+    private int number_of_visits;
     private Date registrationDate;
     @OneToOne
     private User user;
 
     public Entry() { }
+
+    public int getNumber_of_visits() {
+        return number_of_visits;
+    }
+
+    public void setNumber_of_visits(int number_of_visits) {
+        this.number_of_visits = number_of_visits;
+    }
 
     public long getIdEntry() {
         return idEntry;
