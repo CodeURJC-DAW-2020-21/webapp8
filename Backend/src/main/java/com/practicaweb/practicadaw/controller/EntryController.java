82963ef683a851ddf133ce5c8f888b22db89bcc0
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-20 12:24:32 +0100

    EntryController implemented

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/EntryController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/EntryController.java
@@ -1,7 +1,29 @@
 package com.practicaweb.practicadaw.controller;
 
+import com.practicaweb.practicadaw.Service.EntryService;
+import com.practicaweb.practicadaw.Service.UserService;
+import com.practicaweb.practicadaw.model.Entry;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EntryController {
+
+    private final UserService userService;
+    private final EntryService entryService;
+
+    public EntryController(UserService userService, EntryService entryService) {
+        this.userService = userService;
+        this.entryService = entryService;
+    }
+
+    // New entry
+    @PostMapping("/newEntry")
+    public String createEntry(@ModelAttribute Entry entry){
+//        userService.selectById(id);
+        entryService.save(entry);
+        return "redirect:/";
+    }
 }
