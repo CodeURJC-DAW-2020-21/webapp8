1b15d41f9aa9ad0cda45b1fb157766c218712d9e
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-22 02:31:48 +0100

    Tweaks in entry classes

--- Backend/src/main/java/com/practicaweb/practicadaw/Service/EntryService.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/Service/EntryService.java
@@ -1,23 +1,29 @@
 package com.practicaweb.practicadaw.Service;
 
 import com.practicaweb.practicadaw.ServiceInterfaces.EntryServiceInterface;
+import com.practicaweb.practicadaw.auxClasses.auxiliar;
 import com.practicaweb.practicadaw.model.Entry;
+import com.practicaweb.practicadaw.model.User;
 import com.practicaweb.practicadaw.repository.EntryRepository;
+import com.practicaweb.practicadaw.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class EntryService implements EntryServiceInterface {
 
     private final EntryRepository entryRepository;
-    private final UserService userService;
+    private final UserRepository userRepository;
+    private auxiliar aux;
+    private User user1;
 
-    public EntryService(EntryRepository entryRepository, UserService userService) {
+    public EntryService(EntryRepository entryRepository, UserRepository userRepository) {
         this.entryRepository = entryRepository;
-        this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -42,5 +48,12 @@ public class EntryService implements EntryServiceInterface {
         entryRepository.deleteEntryByIdUser(idUser);
     }
 
+
+    @PostConstruct
+    public void examples() {
+//        save(new Entry(1, "Cuales son las mejores criptomonedas para invertir", "Me gustaria que me pudieran decir cuales son algunas de las mejores criptomonedas del mercado.", aux.getActualDate(), userRepository., null));
+//        save(new Entry(2, "Angel", "Fideos", "angel_Fideos", "angel@gmail.com", "hola", "User", aux.getActualDate(), null));
+//        save(new Entry(3, "admin", "admin", "admin", "admin@admin.com", "admin", "admin", aux.getActualDate(), null));
+    }
 
 }
