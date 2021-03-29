634a04e9062945ec186325afb787e37c734f3479
Marcos Rodriguez <marcoszas@gmail.com>
2021-03-30 00:03:52 +0200

    Fixed settings now displayed correctly

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/SettingsController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/SettingsController.java
@@ -1,5 +1,6 @@
 package com.practicaweb.practicadaw.controller;
 
+import com.practicaweb.practicadaw.Service.UserService;
 import com.practicaweb.practicadaw.model.User;
 import com.practicaweb.practicadaw.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,33 +8,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.Principal;
 import java.util.Optional;
 
 @Controller
 public class SettingsController {
 
+    private static final Path IMAGES_FOLDER = Paths.get(System.getProperty("user.dir"), "src/main/resources/static/profileImages");
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @ModelAttribute
     public void addAttributes(Model model, HttpServletRequest request) {
 
-
         Principal principal = request.getUserPrincipal();
+
 
         if(principal != null) {
 
-            Optional<User> user = userRepository.findByName(principal.getName());
+            User user = userService.findByName(principal.getName());
 
             model.addAttribute("logged", true);
             model.addAttribute("userName", principal.getName());
             model.addAttribute("isAdmin", request.isUserInRole("ADMIN"));
-            /*model.addAttribute("surname", user.getSurname());
+            model.addAttribute("firstname", user.getFirstname());
+            model.addAttribute("surname", user.getSurname());
             model.addAttribute("email", user.getEmail());
-            model.addAttribute("nickname", user.getUserName());
-            model.addAttribute("imageFile", user.getImage());*/
+            model.addAttribute("imageFile", user.getImage());
 
 
         } else {
@@ -46,4 +55,20 @@ public class SettingsController {
         return "settings";
     }
 
+    @PostMapping("/update_user")
+    public String updateUser(@ModelAttribute User userUpdate, @RequestParam("imageFile") MultipartFile imageFile, @RequestParam("firtsname") String firstname, @RequestParam("surname") String surname, HttpServletRequest request) throws IOException {
+        Principal principal = request.getUserPrincipal();
+        User user = userService.findByName(principal.getName());
+        if (!imageFile.getOriginalFilename().equals("")) {
+            Path imagePath = IMAGES_FOLDER.resolve(imageFile.getOriginalFilename());
+            imageFile.transferTo(imagePath);
+            userUpdate.setImage(imageFile.getOriginalFilename());
+        } else {
+            userUpdate.setImage(user.getImage());
+        }
+        user.setFirstname(firstname);
+        user.setSurname(surname);
+        userService.save(user);
+        return "redirect:/settings";
+    }
 }
