09159fce423e1759b2b6fa59a97ae7e51bee62bc
Marcos Rodriguez <marcoszas@gmail.com>
2021-04-03 13:31:59 +0200

    Added Csrf check

--- Backend/src/main/java/com/practicaweb/practicadaw/security/SecurityConfiguration.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/security/SecurityConfiguration.java
@@ -61,6 +61,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.logout().logoutUrl("/logout");
         http.logout().logoutSuccessUrl("/");
 
-        http.csrf().disable();
+//        http.csrf().disable();
     }
 }
