79365e45be3fb54291155fea3ecf753691750643
Marcos Rodriguez <marcoszas@gmail.com>
2021-04-06 01:32:17 +0200

    Removed images

--- Backend/src/main/java/com/practicaweb/practicadaw/controller/CriptocurrencyController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/controller/CriptocurrencyController.java
@@ -7,6 +7,7 @@ import com.practicaweb.practicadaw.model.Criptocurrency;
 import com.practicaweb.practicadaw.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class CriptocurrencyController {
@@ -28,17 +30,19 @@ public class CriptocurrencyController {
     }
 
     @PostMapping("/add_cripto")
-    public String add_criptocurrency(@RequestParam String nameCripto, HttpServletRequest request) {
+    public String add_criptocurrency(@RequestParam String nameCripto, HttpServletRequest request, Model model) {
         Principal principal = request.getUserPrincipal();
+        List<Criptocurrency> criptocurrencies = criptocurrencyService.selectAll();
         if (principal != null) {
             User user = userService.findByName(principal.getName()).orElseThrow();
             Criptocurrency cripto = criptocurrencyService.findByNameCripto(nameCripto); //¿Este serie el id de la persona que queremos seguir?
             if (user.getCriptocurrencies().contains(cripto)) {
                 user.removeCript(cripto);
-                cripto.setImage("images/starEmpty.svg");
+//                cripto.setImage("images/starEmpty.svg");
+
             } else {
                 user.addCript(cripto);
-                cripto.setImage("images/star.svg");
+//                cripto.setImage("images/star.svg");
             }
             userService.save(user);
         }
