9e3a86a3dc3485837a77777758b290bb216fd1bc
Adriton1 <adrianlopezcouso@gmail.com>
2021-04-18 20:40:04 +0200

    CryptocurrencyRestController api documentation

--- Backend/src/main/java/com/practicaweb/practicadaw/api/CryptocurrencyRestController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/api/CryptocurrencyRestController.java
@@ -7,6 +7,11 @@ import com.practicaweb.practicadaw.api.user.UserRestController;
 import com.practicaweb.practicadaw.model.Criptocurrency;
 import com.practicaweb.practicadaw.model.Entry;
 import com.practicaweb.practicadaw.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,11 +33,43 @@ public class CryptocurrencyRestController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Get a list of the web app cryptocurrencies.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The list of cryptocurrencies is displayed correctly",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "List of cryptocurrencies not found",
+                    content = @Content
+            )
+    })
     @GetMapping("/")
     public Collection<Criptocurrency> getCryptocurrencies(){
         return criptocurrencyService.selectAll();
     }
 
+
+    @Operation(summary = "Get a cryptocurrency by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "cryptocurrency found",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "cryptocurrency not found",
+                    content = @Content
+            )
+    })
     @GetMapping("{id}")
     public ResponseEntity<Criptocurrency> getCriptocurrency(@PathVariable long id){
         Optional<Criptocurrency> cripto = criptocurrencyService.findById(id);
@@ -40,6 +77,25 @@ public class CryptocurrencyRestController {
         return cripto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+
+    @Operation(summary = "Add a cryptocurrency by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "cryptocurrency added correctly",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "cryptocurrency id not found",
+                    content = @Content
+            )
+    })
     @PostMapping("/{id}/cryptocurrencies")
     public ResponseEntity<Criptocurrency> addFavCrypto(@PathVariable long id, HttpServletRequest request){
         Principal principal = request.getUserPrincipal();
@@ -60,6 +116,25 @@ public class CryptocurrencyRestController {
         }
     }
 
+
+    @Operation(summary = "Delete a favCryptocurrency by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "favCryptocurrency deleted correctly",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "favCryptocurrency id not found",
+                    content = @Content
+            )
+    })
     @DeleteMapping("/{id}/cryptocurrencies")
     public ResponseEntity<Criptocurrency> deleteFavCrypto(@PathVariable long id, HttpServletRequest request){
         Optional<Criptocurrency> cryptoOptional = criptocurrencyService.findById(id);
@@ -81,6 +156,20 @@ public class CryptocurrencyRestController {
     }
 
 
+
+    @Operation(summary = "Get a list of the user's favCryptocurrency.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The list of favCryptocurrency is displayed correctly",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "List of users not found",
+                    content = @Content
+            )
+    })
     @GetMapping("/favCryptocurrency")
     public Collection<Criptocurrency> getFavCryptocurrencies(HttpServletRequest request){
         Principal principal = request.getUserPrincipal();
@@ -89,6 +178,26 @@ public class CryptocurrencyRestController {
         return user.getCriptocurrencies();
     }
 
+
+
+    @Operation(summary = "Delete a cryptocurrency by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "cryptocurrency deleted correctly",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "cryptocurrency id not found",
+                    content = @Content
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Criptocurrency> deleteUser(@PathVariable long id){
         Optional<Criptocurrency> cryptoOptional = criptocurrencyService.findById(id);
