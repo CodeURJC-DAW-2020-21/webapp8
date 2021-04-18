1f06845a201760d145ba9f884c2fee622ec45b79
adrisierra <adriansr2010@gmail.com>
2021-04-18 18:35:32 +0200

    Comment rest controller api documentation

--- Backend/src/main/java/com/practicaweb/practicadaw/api/comment/CommentRestController.java
+++ Backend/src/main/java/com/practicaweb/practicadaw/api/comment/CommentRestController.java
@@ -4,9 +4,15 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.practicaweb.practicadaw.Service.CommentService;
 import com.practicaweb.practicadaw.Service.EntryService;
 import com.practicaweb.practicadaw.Service.UserService;
+import com.practicaweb.practicadaw.api.entry.EntryRestController;
 import com.practicaweb.practicadaw.model.Comment;
 import com.practicaweb.practicadaw.model.Entry;
 import com.practicaweb.practicadaw.model.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +46,22 @@ public class CommentRestController {
     interface Comments extends Comment.Basic{}
     interface CommentsUser extends Comment.Basic, Comment.CommentUser, User.Basic{}
 
+    @Operation(summary = "Get a list of the comments.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The list of comments is displayed correctly",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Comment.Basic.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "List of comments not found",
+                    content = @Content
+            )
+    })
     @JsonView(Comments.class)
     @GetMapping("/")
     public ResponseEntity<Collection<Comment>> getComments(){
@@ -52,6 +74,28 @@ public class CommentRestController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @Operation(summary = "Get a comment by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Comment found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Comment.Basic.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Comment not found",
+                    content = @Content
+            )
+    })
     @JsonView(Comments.class)
     @GetMapping("/{id}")
     public ResponseEntity<Comment> getComment(@PathVariable long id) {
@@ -60,6 +104,27 @@ public class CommentRestController {
         return comment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Get a list of comments by an id user.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Comment found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CommentRestController.CommentsUser.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Comment not found",
+                    content = @Content
+            )
+    })
     @JsonView(CommentsUser.class)
     @GetMapping("/{id}/comment")
     public ResponseEntity<Comment> getUserComment(@PathVariable long id) {
@@ -68,6 +133,18 @@ public class CommentRestController {
         return comment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+
+    @Operation(summary = "Create a new Comment")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Comment created correctly",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Comment.Basic.class)
+                    )}
+            )
+    })
     @PostMapping("/")
     public ResponseEntity<Comment> createComment(@RequestBody CommentDTO commentDTO, HttpServletRequest request) {
         Principal principal = request.getUserPrincipal();
