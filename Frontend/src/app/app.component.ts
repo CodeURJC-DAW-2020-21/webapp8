1f0c2044c104529e6e51022e6f6dc4c81e2b95c0
Oscarprezz <oscar.ramadan.perez@gmail.com>
2021-04-26 17:55:50 +0200

    Forum components structured and added

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component} from '@angular/core';
 
 // interface Entry {
 //   description: string;
@@ -14,21 +14,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   entries: string[] = [];
 
-  constructor(private httpClient: HttpClient){
+  constructor(private httpClient: HttpClient) {
 
   }
 
-  getEntries(page:number) {
+  getEntries(page: number) {
     this.entries = [];
     let url = "/api/entries/?numOfPage=" + page;
     this.httpClient.get(url).subscribe(
       response => {
         let data: any = response;
-        for (let i = 0; i < data.length; i++){
+        for (let i = 0; i < data.length; i++) {
           let entryTitle = data[i].title;
           this.entries.push(entryTitle);
         }
